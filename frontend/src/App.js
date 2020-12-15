c16aff36d1888a81898e7eebac8ac996d3f6c19e
hv6kyh <hv6kyh@gmail.com>
2020-12-16 00:04:42 +0900

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [lists, setLists] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    axios.get("/api/posts").then((resp) => {
+      console.log("resp: ", resp);
+      setLists(resp.data);
+    });
+  }, []);
+
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios.post("/api/posts", { content: value }).then((resp) => {
+      if (resp.data.success) {
+        console.log("resp: ", resp);
+        // setLists([...lists, resp.data]);
+        setLists([...lists, { content: value }]);
+        setValue("");
+      } else {
+        alert("값을 DB에 넣는데 실패했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="container">
+          {lists &&
+            lists.map((list, index) => {
+              return <li key={index}>{list.content}</li>;
+            })}
+          <form className="example" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="입력해주세요..."
+              onChange={changeHandler}
+              value={value}
+            ></input>
+            <button type="submit">확인</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
